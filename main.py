6bc92870fce904e99b53df234752757e021972dd
Ewan <ewanbutterfield@gmail.com>
2023-05-07 15:48:35 -0400

    Searching Stores Proof of Concept

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-# This is a sample Python script.
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+from enum import Enum
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+driver = webdriver.Chrome()
 
 
-def print_hi(name):
+class Stores(Enum):
+    no_frills = "https://www.nofrills.ca/"
+    save_on_foods = "https://www.saveonfoods.com/sm/pickup/rsid/1982/"
+    superstore = "https://www.realcanadiansuperstore.ca/"
+    urban_fare = "https://www.urbanfare.com/sm/pickup/rsid/7615/"
+
+
+# iga is just the worst and doesn't work when the rest do
+#    iga = "https://www.iga.net/en"
+
+
+def main():
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    search_all_stores("Bread")
+    time.sleep(60)
+    driver.quit()
+
+
+def search_product(store_url, product_keywords):
+    driver.switch_to.new_window('tab')
+    driver.get(store_url)
+    search_bar = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "input")))
+
+    search_bar.click()
+    search_bar.clear()
+    search_bar.send_keys(product_keywords)
+    search_bar.send_keys(Keys.RETURN)
+
+
+def search_all_stores(product_keywords):
+    for store in Stores:
+        search_product(store.value, product_keywords)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
